aadc8a0072f08b8692b9b59a5619ba0b5cad5af3
yuvraj372 <149868196+yuvraj372@users.noreply.github.com>
2024-11-07 18:51:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Config:
     API_ID = environ.get("API_ID", "26583504")
     API_HASH = environ.get("API_HASH", "ef9d71b20a1a32669edd7cb458535fae")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7823841864:AAEk0r-97SxwQlHNa8RBgcQyAuHFSORP1sA") 
-    BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    BOT_SESSION = environ.get("BOT_SESSION", "Forward6336bot") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Forwardbot:yuvraj178@forwardbot.xnamt.mongodb.net/?retryWrites=true&w=majority&appName=Forwardbot")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Forwardbot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6471106079').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002399867259'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002304371242") 
